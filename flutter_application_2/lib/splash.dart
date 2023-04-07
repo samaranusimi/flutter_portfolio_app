cf9250af4fa20d945f2b07efe3a4cc80428aebfd
samaranusimi <samarkum185229@gmail.com>
2023-04-08 02:05:47 +0530

    updated

--- flutter_application_2/lib/splash.dart
+++ flutter_application_2/lib/splash.dart
@@ -1,1 +1,25 @@
-import '';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Splash_Screen extends StatefulWidget {
+  const Splash_Screen({super.key});
+
+  @override
+  State<Splash_Screen> createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Image.asset("assets/images/logo.png"),
+      )),
+    );
+  }
+}
