e10502be3be83e918126d79d9a3f5364f652cca0
samaranusimi <samarkum185229@gmail.com>
2023-04-09 16:06:32 +0530

    updated

--- flutter_application_2/lib/pages/contacts.dart
+++ flutter_application_2/lib/pages/contacts.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Contacts extends StatelessWidget {
   const Contacts({super.key});
@@ -16,11 +21,31 @@ class Contacts extends StatelessWidget {
           padding: EdgeInsets.symmetric(vertical: 100, horizontal: 20),
           child: GridView(
               children: [
-                ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Github",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 40)))
+                Container(
+                    child: Link(
+                  target: LinkTarget.self,
+                  uri: Uri.parse("https://github.com/samaranusimi"),
+                  builder: (context, followLink) => ElevatedButton(
+                      onPressed: followLink,
+                      child: Text("Github",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 40))),
+                ))
+                Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 100, horizontal: 20),
+          child: GridView(
+              children: [
+                Container(
+                    child: Link(
+                  target: LinkTarget.self,
+                  uri: Uri.parse("https://github.com/samaranusimi"),
+                  builder: (context, followLink) => ElevatedButton(
+                      onPressed: followLink,
+                      child: Text("Linkedin",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 40))),
+                ))
               ],
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
