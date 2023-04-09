4ef1e39f59d77a18f164434acd0e5d17a5bac6d5
samaranusimi <samarkum185229@gmail.com>
2023-04-09 22:13:00 +0530

    updated

--- flutter_application_2/lib/pages/home_page.dart
+++ flutter_application_2/lib/pages/home_page.dart
@@ -9,74 +9,81 @@ class Home_Page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          elevation: 0,
           title: Text(
             "Protfolio",
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
         ),
-        body: Column(children: [
-          SizedBox(
-            height: 50,
-          ),
-          CircleAvatar(
-            radius: 120,
-            backgroundImage: AssetImage("assets/images/p1.jpeg"),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Row(
-            children: [
-              Text(
-                "Name:",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "Samar Anusimi",
-                style: TextStyle(fontSize: 30),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              Text(
-                "About :",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.blue, Colors.white],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight)),
+          child: Column(children: [
+            SizedBox(
+              height: 50,
+            ),
+            CircleAvatar(
+              radius: 120,
+              backgroundImage: AssetImage("assets/images/p1.jpeg"),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Text(
+                  "Name:",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
-                textAlign: TextAlign.left,
+                Text(
+                  "Samar Anusimi",
+                  style: TextStyle(fontSize: 30),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Text(
+                  "About :",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "I am currently pursuing B.Tech Degree in Computer Science and Engineering from SRMIST, Kattankulathur. As a student i am eager to gain experience in your feild and contribute to your organizaton. ",
+              style: TextStyle(fontSize: 22),
+            ),
+            SizedBox(
+              height: 190,
+            ),
+            ElevatedButton(
+              child: Text(
+                "Next",
+                style: TextStyle(color: Colors.black87),
               ),
-            ],
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Text(
-            "I am currently pursuing B.Tech Degree in Computer Science and Engineering from SRMIST, Kattankulathur. As a student i am eager to gain experience in your feild and contribute to your organizaton. ",
-            style: TextStyle(fontSize: 22),
-          ),
-          SizedBox(
-            height: 190,
-          ),
-          ElevatedButton(
-            child: Text(
-              "Next",
-              style: TextStyle(color: Colors.black87),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => Last_Page()));
+              },
+              style: ElevatedButton.styleFrom(
+                textStyle: TextStyle(fontSize: 20),
+                primary: Colors.amber[900],
+              ),
             ),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context) => Last_Page()));
-            },
-            style: ElevatedButton.styleFrom(
-              textStyle: TextStyle(fontSize: 20),
-              primary: Colors.amber[900],
-            ),
-          ),
-        ])
-        );
+          ]),
+        ));
   }
 }
--- flutter_application_2/lib/pages/last_page.dart
+++ flutter_application_2/lib/pages/last_page.dart
@@ -10,125 +10,136 @@ class Last_Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+      ),
       body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.blue, Colors.purpleAccent],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight)),
           child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 140),
-        child: GridView(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                  color: Colors.black,
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (BuildContext context) => Skills()));
-                        },
-                        child: Text(
-                          "Skills",
-                          style: TextStyle(
-                              fontSize: 35,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          textStyle: TextStyle(fontSize: 20),
-                          primary: Colors.black54,
-                        ),
-                      )
-                    ]),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                  color: Colors.black,
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (BuildContext context) => Projects()));
-                        },
-                        child: Text(
-                          "Projects",
-                          style: TextStyle(
-                              fontSize: 35,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          textStyle: TextStyle(fontSize: 20),
-                          primary: Colors.black54,
-                        ),
-                      )
-                    ]),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                  color: Colors.black,
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (BuildContext context) => Experience()));
-                        },
-                        child: Text(
-                          "Experience",
-                          style: TextStyle(
-                              fontSize: 27,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          textStyle: TextStyle(fontSize: 20),
-                          primary: Colors.black54,
-                        ),
-                      )
-                    ]),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                  color: Colors.black,
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (BuildContext context) => Contacts()));
-                        },
-                        child: Text(
-                          "Contact",
-                          style: TextStyle(
-                              fontSize: 35,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          textStyle: TextStyle(fontSize: 20),
-                          primary: Colors.black54,
-                        ),
-                      )
-                    ]),
-              )
-            ],
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, mainAxisSpacing: 20, crossAxisSpacing: 20)),
-      )
-    ),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 140),
+            child: GridView(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.black,
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (BuildContext context) => Skills()));
+                            },
+                            child: Text(
+                              "Skills",
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              textStyle: TextStyle(fontSize: 20),
+                              primary: Colors.black54,
+                            ),
+                          )
+                        ]),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.black,
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      Projects()));
+                            },
+                            child: Text(
+                              "Projects",
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              textStyle: TextStyle(fontSize: 20),
+                              primary: Colors.black54,
+                            ),
+                          )
+                        ]),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.black,
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      Experience()));
+                            },
+                            child: Text(
+                              "Experience",
+                              style: TextStyle(
+                                  fontSize: 27,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              textStyle: TextStyle(fontSize: 20),
+                              primary: Colors.black54,
+                            ),
+                          )
+                        ]),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.black,
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      Contacts()));
+                            },
+                            child: Text(
+                              "Contact",
+                              style: TextStyle(
+                                  fontSize: 35,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              textStyle: TextStyle(fontSize: 20),
+                              primary: Colors.black54,
+                            ),
+                          )
+                        ]),
+                  )
+                ],
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 20)),
+          )),
     );
   }
 }
